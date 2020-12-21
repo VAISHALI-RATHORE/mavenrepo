2ac2a2a0cda47ddec43333ef04b117c46fccb08b
= <=>
2020-12-21 15:18:46 +0530

    space

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -15,10 +15,10 @@ public class Calc {
 		return num1/num2;
 		}
 	public static void main(String args[]) {
-		System.out.println("addition"+ add(10,20));
-		System.out.println("product"+ prod(70,20));
-		System.out.println("substraction"+ subs(70,20));
-		System.out.println("division"+ div(70,20));
+		System.out.println("addition  "+ add(10,20));
+		System.out.println("product  "+ prod(70,20));
+		System.out.println("substraction  "+ subs(70,20));
+		System.out.println("division  "+ div(70,20));
 	}
 
 }
