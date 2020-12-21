e3781532306e9fe40cd2b7e5789ae6efba88b51e
= <=>
2020-12-21 15:09:26 +0530

    prod method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,8 +8,12 @@ public class Calc {
 	public static int subs(int num1,int num2) {
 	return num1-num2;
 	}
+	public static int prod(int num1,int num2) {
+		return num1*num2;
+		}
 	public static void main(String args[]) {
 		System.out.println("addition"+add(10,20));
+		System.out.println("product"+prod(70,20));
 		System.out.println("substraction"+subs(70,20));
 	}
 
