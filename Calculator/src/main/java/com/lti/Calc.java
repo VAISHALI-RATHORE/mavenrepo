e86c9984602abe29ebd795560187ff8bf17752fb
= <=>
2020-12-21 15:16:32 +0530

    div method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -11,10 +11,14 @@ public class Calc {
 	public static int prod(int num1,int num2) {
 		return num1*num2;
 		}
+	public static int div(int num1,int num2) {
+		return num1/num2;
+		}
 	public static void main(String args[]) {
-		System.out.println("addition"+add(10,20));
-		System.out.println("product"+prod(70,20));
-		System.out.println("substraction"+subs(70,20));
+		System.out.println("addition"+ add(10,20));
+		System.out.println("product"+ prod(70,20));
+		System.out.println("substraction"+ subs(70,20));
+		System.out.println("division"+ div(70,20));
 	}
 
 }
