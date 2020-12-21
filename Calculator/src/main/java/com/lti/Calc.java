c86958b7179a70f1ea1dea903f9b83ae5c6bd037
= <=>
2020-12-21 14:58:46 +0530

    method added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,8 +5,12 @@ public class Calc {
 	public static int add(int num1,int num2) {
 	return num1+num2;
 	}
+	public static int subs(int num1,int num2) {
+	return num1-num2;
+	}
 	public static void main(String args[]) {
-		System.out.println(add(10,20));
+		System.out.println("addition"+add(10,20));
+		System.out.println("substraction"+subs(70,20));
 	}
 
 }
